3897e31d950e9b7e935f1b9aceec27c675426de6
Nico Deshler <nico.deshler@gmail.com>
2021-07-28 20:38:17 -0700

    fixed q0 implementation and doctest

--- inference.py
+++ inference.py
@@ -94,7 +94,7 @@ class DiscreteDistribution(dict):
         >>> dist['d'] = 0
         >>> N = 100000.0
         >>> samples = [dist.sample() for _ in range(int(N))]
-        >>> print(samples)>>> round(samples.count('a') * 1.0/N, 1)  # proportion of 'a'
+        >>> round(samples.count('a') * 1.0/N, 1)  # proportion of 'a'
         0.2
         >>> round(samples.count('b') * 1.0/N, 1)
         0.4
